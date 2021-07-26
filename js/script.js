d8fde6231c6fbdf3f647b45237821530c686a08a
Cherryl Osewe <cherrylwendy03@gmail.com>
2021-07-26 10:03:24 +0300

    adding a user input verification

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-$(document).ready(function(){
+//business logic
+
+
+  
+
+
+
+
+
+
+
+
+//user interface
+$(document).ready(function () {
   $(".clickabledsg").click(function () {
     $(".design-showing").toggle();
     $(".design-hidden").toggle();
@@ -12,3 +25,25 @@ $(document).ready(function(){
     $(".product-hidden").toggle();
   });
 });
+
+function onSubmit(event){
+
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let inputText = document.getElementById('input-text').value;
+
+  if (name==null || name =="" ){
+    alert("Please input your name")
+  }
+  else if (email==null || email==""  ){
+    alert("Please input your email")
+  }
+  else if (inputText == null || inputText == "") {
+    alert("Please iadd a message")
+  }
+  else{
+  alert(name + " we have received your message.Thank you for reaching out to us.")
+  }
+
+  event.preventDefault()
+}
